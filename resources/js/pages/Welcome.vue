1807b804951c80073df068960bde6e5acfd99545
Tead <teadmarvin@gmail.com>
2025-11-13 06:48:47 +0800

    initial commit

---
+++ resources/js/pages/Welcome.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { dashboard, login, register } from '@/routes';
+import { Head, Link } from '@inertiajs/vue3';
+
+withDefaults(
+    defineProps<{
+        canRegister: boolean;
+    }>(),
+    {
+        canRegister: true,
+    },
+);
+</script>
+
+<template>
+    <Head title="Welcome">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+
+    <main class="fixed inset-0">
+        <img
+            src="/storage/images/findjerks.png"
+            alt="FindJerks Landing Image"
+            class="h-full w-full object-cover"
+        />
+    </main>
+
+</template>
